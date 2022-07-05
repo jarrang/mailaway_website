7b09b92d8f86ba0d590d70aa03423369966802f5
Conor Wilson <conor@jarrang.com>
2022-07-05 14:52:44 +0100

    Main website

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("./src/assets/");
+    return {
+        dir: {
+            input: "src",
+            output: "build"
+        },
+    };
+};
